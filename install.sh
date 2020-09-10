aeb8003802263c563d769736b5c83310de4e859e
Aki-7 <aki.develop8128@gmail.com>
2020-09-10 22:47:13 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+
+NOTIM_HOME=~/.notim
+
+if [ -e $NOTIM_HOME ]; then
+  echo "~/.notim is already exist."
+  echo "remove ~/.notim to use notim."
+  exit 0
+fi
+
+mkdir -p ~/.notim
+
+cd $NOTIM_HOME
+
+git clone https://github.com/Aki-7/notim.git .
+
+mkdir note
