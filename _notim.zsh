e2df772b2cf83b01acd13a7eccf84a6930a136f6
Aki-7 <aki.develop8128@gmail.com>
2020-09-11 01:44:32 +0900

    update auto complete func

--- _notim.zsh
+++ _notim.zsh
@@ -2,7 +2,11 @@ NOTIM_HOME=${NOTIM_HOME:-~/.notim}
 NOTIM_NOTE_ROOT=${NOTIM_HOME%/}/note
 
 _notim() {
-  _files -W $NOTIM_NOTE_ROOT
+  _arguments '(- *): :->modes'
+  case $state in
+    modes)
+      _files -W $NOTIM_NOTE_ROOT;;
+  esac
 }
   
 compdef _notim notim
